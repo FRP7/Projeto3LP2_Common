e0b94a8cc34f6f3d6f9f8a5f7dec87072ac23a5b
FranciscoPires <frelvaspires@gmail.com>
2021-01-07 11:45:27 +0000

    Método Start adicionado ao GameState

--- GameState.cs
+++ GameState.cs
@@ -17,19 +17,22 @@ namespace Common
         /// </summary>
         public bool IsGameOver { get; private set; }
 
+        public void Start()
+        {
+            gameObject[0] = new BlackPiece();
+            gameObject[1] = new WhitePiece();
+            gameObject[2] = new Board();
+            IsGameOver = false;
+            gameObject[0].Start();
+            gameObject[1].Start();
+            gameObject[2].Start();
+        }
+
         public void Update()
         {
             gameObject[2].Update();
             gameObject[1].Update();
             gameObject[0].Update();
-        }
-
-        public GameState()
-        {
-            IsGameOver = false;
-            /*gameObject[0] = new BlackPiece();
-            gameObject[1] = new WhitePiece();
-            gameObject[2] = new Board();*/
         }
     }
 }
